6a61b7734ad02374578ee3ffa006bd957699a0f4
nishacodes <nishabatra27@gmail.com>
2013-11-19 22:52:11 -0500

    write spec and solution for remove_nils

---
+++ TDDiteration/remove_nils_spec.rb
@@ -0,0 +1,7 @@
+require "./remove_nils_solution"
+
+describe "#some_method"  do
+	it "should return an array in which new elements are the square of the original elements" do 
+		expect{some_method}.to_not raise_exception
+	end
+end
