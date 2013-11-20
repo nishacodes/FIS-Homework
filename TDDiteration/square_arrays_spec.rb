8d743cd3519f13c1436aef3c6e810a3e979bbb59
nishacodes <nishabatra27@gmail.com>
2013-11-19 21:14:39 -0500

    change the () to {} to make the second test pass

--- TDDiteration/square_arrays_spec.rb
+++ TDDiteration/square_arrays_spec.rb
@@ -5,9 +5,9 @@ describe "#square_array"  do
 		expect(square_array([2,4,6])).to eq([4,16,36])
 	end
 
-	# this one is not working.
+	# this one works only if you use the { } for some reason.
 	it "should get an error if a non-numeric array is passed in" do
-		expect(square_array(["a","b","c"])).to raise_error
+		expect{square_array(["a","b","c"])}.to raise_error
 	end
 
 	it "should return an empty array if an empty array is passed in" do
