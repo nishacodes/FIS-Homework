114ee68bb2cdef165b27f0f0aaf2065d560fdfb5
nishacodes <nishabatra27@gmail.com>
2013-11-19 21:05:03 -0500

    write spec and solution for square_arrays; error test currently not working

--- TDDiteration/square_arrays_solution.rb
+++ TDDiteration/square_arrays_solution.rb
@@ -2,5 +2,8 @@
 # Return an array that contains the squares of all elements in an array?
 
 def square_array(some_array)
-  # code goes here
+  some_array.collect do |number|
+  	number ** 2
+  end
 end
+
---
+++ TDDiteration/square_arrays_spec.rb
@@ -0,0 +1,18 @@
+require "./square_arrays_solution"
+
+describe "#square_array"  do
+	it "should return an array in which new elements are the square of the original elements" do 
+		expect(square_array([2,4,6])).to eq([4,16,36])
+	end
+
+	# this one is not working.
+	it "should get an error if a non-numeric array is passed in" do
+		expect(square_array(["a","b","c"])).to raise_error
+	end
+
+	it "should return an empty array if an empty array is passed in" do
+		expect(square_array([])).to eq([])
+	end
+
+end
+
