5e943265ed2913b5833ab4ff5c4ef77499d231a5
nishacodes <nishabatra27@gmail.com>
2013-11-20 21:26:29 -0500

    write what to test in spec file

--- green_grocer/green_grocer_spec.rb
+++ green_grocer/green_grocer_spec.rb
@@ -1,4 +1,36 @@
 require "./green_grocer"
 
-describe "#checkout" do 
+describe "#discounts" do 
+
+
+	it "should apply 3x the discount if customer has 2 same coupons" do
+
+	end
+
+	it "should apply 20% discount to all cleanance items" do
+	end
+
+	it "should apply a $10 discount if no items are more than $5" do
+	end
+
 end
+
+describe "#checkout" do 
+
+	it "should have the same number of items in the original cart" do
+
+	end
+
+	it "should return a value <= equal to the total cost of all the individual items" do
+
+	end
+
+	it "should incorporate all the discounts calculated" do
+
+	end
+
+
+
+end
+
+
